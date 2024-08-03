4d7c4d6fa51f93a56191cf4b4de085320c504fc8
Maxime <maxdevvynter@gmail.com>
2024-08-03 21:20:45 +0200

    Save a new Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Hello World!");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (
+      !req.body.title || 
+      !req.body.author || 
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+          message: "All fields are required: title, author, publishYear",
+        });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
