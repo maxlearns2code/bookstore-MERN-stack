974be6c44039cf36ee04b037a74102559eb4eb0e
Maxime <maxdevvynter@gmail.com>
2024-08-03 22:49:42 +0200

    Update a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,10 @@ app.get("/", (req, res) => {
 
 app.post("/books", async (req, res) => {
   try {
-    if (
-      !req.body.title || 
-      !req.body.author || 
-      !req.body.publishYear
-    ) {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(400).send({
-          message: "All fields are required: title, author, publishYear",
-        });
+        message: "All fields are required: title, author, publishYear",
+      });
     }
     const newBook = {
       title: req.body.title,
@@ -51,9 +47,29 @@ app.get("/books", async (req, res) => {
 
 app.get("/books/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const book = await Book.findById(id);
     return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "All fields are required: title, author, publishYear",
+      });
+    }
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body)
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book updated successfully" });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
